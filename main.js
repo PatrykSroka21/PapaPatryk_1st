8b9fc3813bb2577d41d77e5599605084ba9a2b0c
PatrykSroka21 <patryk.sroka01@gmail.com>
2020-10-01 14:07:03 +0200

    hjhjf

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = "Patrick"
+let date = "today" 
+
+if (date = "today") {
+    console.log(name);
+    let name = "PapaPatrick"
+
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i == 69, i == 42) {
+        console.log("GAY")
+    }
+    
+}
